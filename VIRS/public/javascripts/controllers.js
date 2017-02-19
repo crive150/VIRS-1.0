8cd005eaa11e9473d8798ccaddfe6f28de0f4c18
camilohel777 <camilo.rive007@gmail.com>
2017-02-18 19:14:26 -0500

    adding controllers

--- VIRS/public/javascripts/controllers.js
+++ VIRS/public/javascripts/controllers.js
@@ -12,10 +12,14 @@ app.controller('K1Ctrl', [
 function($scope, $stateParams){
     $scope.test = 'Hello world!';
     $scope.words = [
-    {title: 'word 1', upvotes: 5},
-    {title: 'word 2', upvotes: 2},
-    {title: 'word 3', upvotes: 15},
-    {title: 'word 4', upvotes: 9},
-    {title: 'word 5', upvotes: 4}
+    {title: 'word 1', definition: "definition 1", frequency: 1},
+    {title: 'word 2', definition: "definition 2", frequency: 2},
+    {title: 'word 3', definition: "definition 3", frequency: 3},
+    {title: 'word 4', definition: "definition 4", frequency: 4},
+    {title: 'word 5', definition: "definition 5", frequency: 5}
   ];
+
+  $scope.addWord = function() {
+    $scope.words.push({title: 'Technology', definition: 'Application of scientific knowledge', frequency: 1})
+  }
 }]);
